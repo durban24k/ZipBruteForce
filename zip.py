3ecc99c2639424cf1e0e239ecea449a7c4d44b19
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-09-27 21:08:49 +0300

    v1.0

---
+++ zip.py
@@ -0,0 +1,24 @@
+import zipfile
+
+char_list='abcdefghijklmnopqrstuvwxyz'
+complete=[]
+
+for current in range(4):
+     a=[i for i in char_list]
+     for x in range(current):
+          a=[y+i for i in char_list for y in a]
+     complete=complete+a
+
+z=zipfile.ZipFile('#') # this is where you input the filepath for the zipfile
+
+tries=0
+
+for password in complete:
+     try:
+          tries+=1
+          z.setpassword(password.encode('ascii'))
+          z.extractall()
+          print(f'Password was found after {tries} tries!!! The password is {password}')
+          break
+     except:
+          pass
